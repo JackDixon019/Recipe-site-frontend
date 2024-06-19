74cfc58f19801d0d4950f55755722844f4a3fd4d
JackDixon019 <13709@coderacademy.edu.au>
2024-06-19 17:47:44 +1000

    CLEANING: removed placeholder recipe

--- src/routes/recipeLoader.ts
+++ src/routes/recipeLoader.ts
@@ -20,53 +20,6 @@ export interface TypeRecipe {
     description: string;
 }
 
-const recipes = [
-    {
-        id: "0",
-        title: "Spaghetti and Meatballs",
-        ingredients: [
-            {
-                count: 500,
-                measurement: "g",
-                name: "Spaghetti",
-            },
-            {
-                count: 500,
-                measurement: "g",
-                name: "Beef Mince",
-            },
-            {
-                count: 400,
-                measurement: "mL",
-                name: "Crushed Tomatoes",
-            },
-            {
-                count: 1,
-                measurement: "clove",
-                name: "Garlic",
-            },
-            {
-                count: 35,
-                measurement: "g",
-                name: "Butter",
-            },
-            { name: "Basil" },
-            { name: "Parmesan cheese" },
-        ],
-        instructions: [
-            "Combine salt, pepper, and meat into meatballs",
-            "Add butter, crushed tomatoes, and onion to pot",
-            "Bring pot to boil, then simmer for 45 minutes",
-            "While sauce is simmering, fry meatballs for 3 minutes per side until browned",
-            "Boil pasta according to packet instructions",
-            "Serve spaghetti in bowl, and top with meatball and tomato sauce. Garnish with basil and parmesan",
-        ],
-        time: 60,
-        description:
-            "Delicious home-cooked dish, a staple of italian-american cooking. This savoury delight combines meatballs, tomato, and pasta to create a scrumptious feast that's enjoyable by all",
-    },
-];
-
 async function getRecipe(recipeId: string): Promise<TypeRecipe | undefined> {
     // return await recipes.find((recipe) => recipe.id === recipeId);
     let url = import.meta.env.VITE_BACK_END_URL + "recipes/" + recipeId;
